7cc5870591e1639540928e6ed5c455900cff15da
chiii111 <100507994+chiii111@users.noreply.github.com>
2022-03-04 01:26:32 +0800

    addBtn_goToTop

--- 220208_jq.js
+++ 220208_jq.js
@@ -1,16 +1,12 @@
 $(function(){
     
-    //-------------------------------------------sec_mainNav-------------------------------------------
-    // -------------------------------------------sec_mainKv-------------------------------------------
-    let $systemColor_color = $(".systemColor_color");
-    let $workBg_systemColor_item = $(".workBg_systemColor_item");
-    let $systemColor_hex = $(".workBg_systemColor_item").find("span").text();
-    console.log($systemColor_hex);
-    $systemColor_color.css("background-color",$systemColor_hex);
-
-
-    // $(".workBg_systemColor_item").click(function(){
-    //     let workBg_systemColor_index = $(this).index();
-    //     console.log(workBg_systemColor_index)
-    // })
+    let $window = $(window)
+    let $goToTop_btn = $(".goToTop_btn");
+    let $mainNavNextSec = $(".sec_mainNav + section");
+    let goToTopHeight = $mainNavNextSec.offset().top;
+    console.log(goToTopHeight)
+    $goToTop_btn.click(function(){
+        $window.scrollTop(goToTopHeight);
+    })
+    
 })
